36c63df66e019eaa8a1707f824826195509b959f
imran3809 <imr3809@gmail.com>
2016-10-17 23:21:59 +0600

    Initial commit

--- app/src/main/java/com/app/appathon/blooddonateapp/activities/MainActivity.java
+++ app/src/main/java/com/app/appathon/blooddonateapp/activities/MainActivity.java
@@ -1,37 +1,29 @@
-package com.app.appathon.blooddonateapp;
+package com.app.appathon.blooddonateapp.activities;
 
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-import android.widget.CompoundButton;
 import android.widget.Toast;
 
-import com.app.appathon.blooddonateapp.utils.CrossfadeWrapper;
+import com.app.appathon.blooddonateapp.R;
 import com.mikepenz.crossfader.Crossfader;
-import com.mikepenz.crossfader.util.UIUtils;
 import com.mikepenz.fontawesome_typeface_library.FontAwesome;
 import com.mikepenz.google_material_typeface_library.GoogleMaterial;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.MiniDrawer;
 import com.mikepenz.materialdrawer.holder.BadgeStyle;
-import com.mikepenz.materialdrawer.interfaces.OnCheckedChangeListener;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.SectionDrawerItem;
-import com.mikepenz.materialdrawer.model.SwitchDrawerItem;
-import com.mikepenz.materialdrawer.model.ToggleDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.Nameable;
-import com.mikepenz.octicons_typeface_library.Octicons;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Drawer result = null;
     private MiniDrawer miniResult = null;
     private Crossfader crossFader;
     @Override
@@ -40,12 +32,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Handle Toolbar
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         //set the back arrow in the toolbar
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        result = new DrawerBuilder()
+        final Drawer result = new DrawerBuilder()
                 .withActivity(this)
                 .withToolbar(toolbar)
                 .withTranslucentStatusBar(false)
@@ -58,9 +50,7 @@ public class MainActivity extends AppCompatActivity {
                         new SectionDrawerItem().withName(R.string.drawer_item_section_header),
                         new SecondaryDrawerItem().withName(R.string.drawer_item_open_source).withIcon(FontAwesome.Icon.faw_github),
                         new SecondaryDrawerItem().withName(R.string.drawer_item_contact).withIcon(GoogleMaterial.Icon.gmd_format_color_fill).withTag("Bullhorn"),
-                        new DividerDrawerItem(),
-                        new SwitchDrawerItem().withName("Switch").withIcon(Octicons.Icon.oct_tools).withChecked(true).withOnCheckedChangeListener(onCheckedChangeListener),
-                        new ToggleDrawerItem().withName("Toggle").withIcon(Octicons.Icon.oct_tools).withChecked(true).withOnCheckedChangeListener(onCheckedChangeListener)
+                        new DividerDrawerItem()
                 ) // add the items we want to use with our Drawer
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
 
@@ -72,42 +62,8 @@ public class MainActivity extends AppCompatActivity {
                         return false;
                     }
                 })
-                .withGenerateMiniDrawer(true)
                 .withSavedInstance(savedInstanceState)
                 // build only the view of the Drawer (don't inflate it automatically in our layout which is done with .build())
-                .buildView();
-
-        //the MiniDrawer is managed by the Drawer and we just get it to hook it into the Crossfader
-        miniResult = result.getMiniDrawer();
-
-        //get the widths in px for the first and second panel
-        int firstWidth = (int) UIUtils.convertDpToPixel(300, this);
-        int secondWidth = (int) UIUtils.convertDpToPixel(72, this);
-
-        //create and build our crossfader (see the MiniDrawer is also builded in here, as the build method returns the view to be used in the crossfader)
-        //the crossfader library can be found here: https://github.com/mikepenz/Crossfader
-        crossFader = new Crossfader()
-                .withContent(findViewById(R.id.crossfade_content))
-                .withFirst(result.getSlider(), firstWidth)
-                .withSecond(miniResult.build(this), secondWidth)
-                .withSavedInstance(savedInstanceState)
                 .build();
-
-        //define the crossfader to be used with the miniDrawer. This is required to be able to automatically toggle open / close
-        miniResult.withCrossFader(new CrossfadeWrapper(crossFader));
-
-        //define a shadow (this is only for normal LTR layouts if you have a RTL app you need to define the other one
-        crossFader.getCrossFadeSlidingPaneLayout().setShadowResourceLeft(R.drawable.material_drawer_shadow_left);
     }
-
-    private OnCheckedChangeListener onCheckedChangeListener = new OnCheckedChangeListener() {
-        @Override
-        public void onCheckedChanged(IDrawerItem drawerItem, CompoundButton buttonView, boolean isChecked) {
-            if (drawerItem instanceof Nameable) {
-                Log.i("material-drawer", "DrawerItem: " + ((Nameable) drawerItem).getName() + " - toggleChecked: " + isChecked);
-            } else {
-                Log.i("material-drawer", "toggleChecked: " + isChecked);
-            }
-        }
-    };
 }
---
+++ app/src/main/java/com/app/appathon/blooddonateapp/fragments/LocatingDonors.java
@@ -0,0 +1,126 @@
+package com.app.appathon.blooddonateapp.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.app.appathon.blooddonateapp.R;
+import com.app.appathon.blooddonateapp.adapter.TabsAdapter;
+import com.app.appathon.blooddonateapp.model.TabsItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.neokree.materialtabs.MaterialTab;
+import it.neokree.materialtabs.MaterialTabHost;
+import it.neokree.materialtabs.MaterialTabListener;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LocatingDonors#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LocatingDonors extends Fragment implements MaterialTabListener {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    MaterialTabHost tabHost;
+    ViewPager viewPager;
+    private List<TabsItem> mTabs = new ArrayList<>();
+    private TabsAdapter tAdapter;
+
+    public LocatingDonors() {
+        // Required empty public constructor
+        createTabsItem();
+    }
+
+    private void createTabsItem() {
+        mTabs.add(new TabsItem("Available", new AvailableDonorsFragment()));
+        mTabs.add(new TabsItem("All", new AllDonorsFragment()));
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LocatingDonors.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LocatingDonors newInstance(String param1, String param2) {
+        LocatingDonors fragment = new LocatingDonors();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_locating_donors, container, false);
+        viewPager = (ViewPager) rootView.findViewById(R.id.viewPager);
+
+         //Set an Adapter for the View Pager
+        tAdapter = new TabsAdapter(getChildFragmentManager(),mTabs);
+        viewPager.setOffscreenPageLimit(mTabs.size());
+        viewPager.setAdapter(tAdapter);
+        viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                // when user do a swipe the selected tab change
+                tabHost.setSelectedNavigationItem(position);
+            }
+        });
+
+        //Adding TabHost
+        tabHost = (MaterialTabHost) rootView.findViewById(R.id.materialTabHost);
+        // insert all tabs from pagerAdapter data
+        for (int i = 0; i < tAdapter.getCount(); i++) {
+            tabHost.addTab(
+                    tabHost.newTab()
+                            .setText(tAdapter.getPageTitle(i))
+                            .setTabListener(this)
+            );
+        }
+        return rootView;
+    }
+
+    @Override
+    public void onTabSelected(MaterialTab tab) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabReselected(MaterialTab tab) {
+
+    }
+
+    @Override
+    public void onTabUnselected(MaterialTab tab) {
+
+    }
+}
